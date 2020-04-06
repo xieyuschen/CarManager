6026f1953e7e1a2296e2ff7b4fe1ea97047d0734
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-04-07 00:07:38 +0800

    understand how format works

--- CarManager/main.cpp
+++ CarManager/main.cpp
@@ -9,12 +9,23 @@
 #include<regex>
 using namespace std;
 int main() {
-	string test = "dog hello cat car target";
-	regex r("^car");
-	smatch results;
-	regex_search(test, results, r);
-	cout << results.str();
+	//这里的序号是子正则式的序号
+	string fmt = "$2.$6.$8";
+	//这里单斜杠都要写成双斜杠，进行转义
+	string phone = "(\\()?(\\d{3})(\\))?([-. ])?(\\s)?(\\d{3})([-. ])?(\\d{4})";
+	regex r(phone);
+	string s;
+	while (getline(cin, s)) {
+		cout << regex_replace(s, r, fmt)<<endl;
+	}
+	return 0;
 }
+
+//string test = "dog hello cat car target";
+//regex r("^car");
+//smatch results;
+//regex_search(test, results, r);
+//cout << results.str();
 
 
 //	string file("1.csv");
