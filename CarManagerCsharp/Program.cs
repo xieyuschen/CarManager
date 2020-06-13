d51ed40ca5d8d73023acaf2b964d731ce9653901
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-06-14 00:47:14 +0800

    call C++ dll in C# Console by change the Target plateform in prosperity

---
+++ CarManagerCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace CarManagerCsharp
+{
+    class Program
+    {
+
+        [DllImport(@"E:\Code\CarManager\Debug\CarManagerDll.dll",EntryPoint ="add", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int add(int a,int b);
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 1, b = 2;
+                int p = add(a, b);
+                Console.WriteLine(p);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ CarManagerDll/Add.cpp
@@ -0,0 +1,5 @@
+extern "C" {
+	__declspec(dllexport) int __stdcall add(int a, int b) {
+		return a + b;
+	}
+}
