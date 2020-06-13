f663228506ae609b2599ad762ff4208914bee53d
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-06-13 18:00:20 +0800

    add extern c but with wrong when define factory

---
+++ CarManagerDll/Factory.cpp
@@ -0,0 +1,22 @@
+#include"Factory.h"
+#include"CsvReader.h"
+#include"Info.h"
+#include"TimeMe.h"
+using namespace std;
+CarManagerCommon::CsvReader* CreateCsvReader() {
+	return new CarManagerCommon::CsvReader();
+}
+CarManagerCommon::CsvReader* CreateCsvReader(string s) {
+	return new CarManagerCommon::CsvReader(s);
+}
+CarManagerCommon::CsvReader* CreateCsvReader(string s,char c) {
+	return new CarManagerCommon::CsvReader(s,c);
+}
+
+CarManagerCommon::Info* CreateInfo(const vector<string>& p, const list<vector<string>>& lists) {
+	return new CarManagerCommon::Info(p, lists);
+}
+
+CarManagerTypes::TimeMe* CreateTimeMe(string year, string month, string day, string hour, string minite, string second) {
+	return new CarManagerTypes::TimeMe(year,month,day,hour,minite,second);
+}
