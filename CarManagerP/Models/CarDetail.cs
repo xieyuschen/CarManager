48accf47627231f9e0227b3332e9bb5d176f8460
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-06-15 14:26:16 +0800

    add Asp.net and try to use funciton in dll

---
+++ CarManagerP/Models/CarDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarManagerP.Models
+{
+    public class CarDetail
+    {
+		public int CarNum;
+		public string CarPlateId;
+		public char CarType;
+		public string Name;
+		public string GearType;
+		public float Price;
+		public string State;
+	}
+}
---
+++ CarManagerP/Pages/CarDetails/Details.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CarManagerP.Models;
+using System.Runtime.InteropServices;
+
+namespace CarManagerP.Pages.CarDetails
+{
+    public class DetailsModel : PageModel
+    {
+        //https://localhost:44365/CarDetails/details
+        [DllImport(@"E:\Code\CarManager\Debug\PureCdll.dll",CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static extern string CsvReader([MarshalAs(UnmanagedType.LPStr)] string path, int row);
+
+        [BindProperty]
+        public CarDetail CarDetail { get; set; }
+        public DetailsModel()
+        {
+            var str = CsvReader("../车辆基本信息表.csv", 1);
+
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
