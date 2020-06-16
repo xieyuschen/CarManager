1b542bb2da2a4dceedb40da1e1ff37fb8dda0411
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-06-16 16:37:23 +0800

    filter cars by name

--- CarManagerP/Pages/Detail/Delete.cshtml.cs
+++ CarManagerP/Pages/Detail/Delete.cshtml.cs
@@ -7,14 +7,35 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CarManagerP.Data;
 using CarManagerP.Models;
+using CarManagerP.Services;
 
 namespace CarManagerP.Pages.Detail
 {
     public class DeleteModel : PageModel
     {
+        public List<CarDetail> Details { get; set; }
 
         public DeleteModel()
         {
+            const string path = @"..\车辆基本信息表.csv";
+            Details = new List<CarDetail>();
+            var lists = FileService.ReadFromFile(path);
+            lists.RemoveAt(0);
+            foreach (var item in lists)
+            {
+                var strs = item.Split("\t");
+                Details.Add(new CarDetail
+                {
+                    CarNum = int.Parse(strs[0]),
+                    CarPlateId = strs[1],
+                    CarType = strs[2],
+                    Name = strs[3],
+                    GearType = strs[4],
+                    Price = float.Parse(strs[5]),
+                    State = strs[6]
+                });
+            }
+
         }
 
         [BindProperty]
@@ -22,36 +43,34 @@ namespace CarManagerP.Pages.Detail
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-            //CarDetail = await _context.CarDetail.FirstOrDefaultAsync(m => m.ID == id);
+            CarDetail = Details.Find(t => t.CarNum == int.Parse(id));
 
-            //if (CarDetail == null)
-            //{
-            //    return NotFound();
-            //}
+            if (CarDetail == null)
+            {
+                return NotFound();
+            }
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string id)
         {
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
-            //CarDetail = await _context.CarDetail.FindAsync(id);
+            var t = Details.Find(t => t.CarNum == CarDetail.CarNum);
+            Details.Remove(t);
+            const string path = @"..\车辆基本信息表.csv";
 
-            //if (CarDetail != null)
-            //{
-            //    _context.CarDetail.Remove(CarDetail);
-            //    await _context.SaveChangesAsync();
-            //}
-
+            FileService.RewriteFile<CarDetail>(path, Details);
             return RedirectToPage("./Index");
+
         }
     }
 }
