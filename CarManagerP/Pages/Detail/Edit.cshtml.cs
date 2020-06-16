7c059107447617161623f8aed558025987b3f479
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-06-17 03:03:20 +0800

    remove SearchStirng part in cshtml

--- CarManagerP/Pages/Detail/Edit.cshtml.cs
+++ CarManagerP/Pages/Detail/Edit.cshtml.cs
@@ -20,23 +20,8 @@ namespace CarManagerP.Pages.Detail
         public EditModel()
         {
             const string path = @"..\车辆基本信息表.csv";
-            Details = new List<CarDetail>();
             var lists = FileService.ReadFromFile(path);
-            lists.RemoveAt(0);
-            foreach (var item in lists)
-            {
-                var strs = item.Split("\t");
-                Details.Add(new CarDetail
-                {
-                    CarNum = int.Parse(strs[0]),
-                    CarPlateId = strs[1],
-                    CarType = strs[2],
-                    Name = strs[3],
-                    GearType = strs[4],
-                    Price = float.Parse(strs[5]),
-                    State = strs[6]
-                });
-            }
+            Details = InitializeService.InitializeCarDetail(lists);
 
         }
         public async Task<IActionResult> OnGetAsync(string id)
